9cfbd91fcc51ad4b4416b790a6cc58e3bcd89028
Ericode254 <jilloerick6@gmail.com>
2024-11-09 03:57:53 +0300

    add a hambugger icon for mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,14 +20,12 @@ const Navbar: React.FC = () => {
 
         {/* Hamburger Icon for Mobile */}
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="w-6 h-6 bg-[#d79921] focus:outline-none">
-            <svg className="w-6 h-6" fill="#fff" viewBox="0 0 24 24">
-              <path d="M4 6h16M4 12h16M4 18h16" />
+          <button onClick={toggleMenu} className="w-8 h-8 focus:outline-none">
+            <svg className="w-6 h-6 text-[#fabd2f]" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" />
             </svg>
           </button>
-        </div>
-
-        {/* Links */}
+        </div>       {/* Links */}
         <ul className={`md:flex md:space-x-6 ${isOpen ? 'block' : 'hidden'} absolute md:relative top-full left-0 w-full md:w-auto bg-[#282828] md:bg-transparent rounded-b-lg shadow-md md:shadow-none p-4 md:p-0`}>
           <li>
             <a href="/about" className="block md:inline-block text-center py-2 hover:text-[#fabd2f] transition-colors duration-300">
